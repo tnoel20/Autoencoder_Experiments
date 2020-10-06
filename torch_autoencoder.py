2e4c43444db24986949398e218cef6e40ebcf0a2
Thomas Noel <noelt@ib-compute-dgx2-5.hpc.engr.oregonstate.edu>
2020-10-06 11:52:39 -0700

    Created .gitignore

--- torch_autoencoder.py
+++ torch_autoencoder.py
@@ -3,3 +3,14 @@ import numpy as np
 
 # Implement autoencoder with several different latent
 # layer sizes. Train on CIFAR-10.
+
+class Autoencoder(nn.module):
+    def __init__(self, **kwargs):
+        pass
+
+
+def main():
+    print("Hello from DGX2!\n")
+
+if __name__ == '__main__':
+    main()
